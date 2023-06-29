ff35f8d13ec923b1ddb4a611d802c1817eea738a
SheilAPK <karaulov.z@mail.ru>
2023-06-29 12:09:32 +0300

    In work

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,12 +1,35 @@
-/*
 $(document).ready(
     function(){
-        $('.header__burger').click(
+        // $('.header__burger').click(
+        //     function (event) {
+        //         $('div.header__burger').toggleClass('header__burger_close');
+        //         $('nav.header__menu').toggleClass('header__menu_phone');
+        //     }
+        // );
+
+        $('.menu__name').click(
             function (event) {
-                $('div.header__burger').toggleClass('header__burger_close');
-                $('nav.header__menu').toggleClass('header__menu_phone');
+                subMenu = event.target.nextElementSibling; // Под.меню
+                if(subMenu) {
+                    $(subMenu).toggleClass('open');
+                }
+
+                // Закрываем все подменю
+                $(subMenu).removeClass('novisible');
+                $(subMenu).find('.sub-sub-menu').removeClass('open');
             }
-        );
+        )
+
+        $('.sub-menu__name').click(
+            function (event) {
+                subMenu = event.target.nextElementSibling; // Под.меню
+                if(subMenu) {
+                    $(subMenu).toggleClass('open');
+                }
+                menu = event.target.parentNode.parentNode;
+                $(menu).toggleClass('novisible');
+            }
+        )
     }
 );
-*/
+
